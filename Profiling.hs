5897f2eb858528dfc88c4fe0ad08c241b808dae9
Inari Listenmaa <inari.listenmaa@gmail.com>
2015-04-09 21:17:44 +0200

    some changes

--- Profiling.hs
+++ Profiling.hs
@@ -14,7 +14,8 @@ main = do
       [p, l, s] -> do
         pgf <- readPGF p 
         let morphAnaOutput = fromMaybe "error in morphAna" $ (MA.parseString pgf) <$> (readLanguage l) <*> pure s
-        let morphDisamOutput = fromMaybe "error in morphDisam" $ (MD.parseString pgf) <$> (readLanguage l) <*> pure s
+        let morphDisamOutput = fromMaybe "error in morphDisam" $ (MD.parseString pgf) <$> (readLanguage l) <*> pure morphAnaOutput
+        
         putStrLn morphDisamOutput
       _ -> putStrLn "usage: ./Main pgf lang str"
      
